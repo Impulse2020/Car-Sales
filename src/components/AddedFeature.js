16151860429fd260c9d0a5883e582223a1238b72
Impulse2020 <irishht@gmail.com>
2020-05-12 17:42:49 -0400

    Adds onclicks and functions

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -4,10 +4,12 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick= {remove} className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
-
+const  remove = event =>{
+  console.log(event.target)
+}
 export default AddedFeature;
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -4,10 +4,12 @@ const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick= {add} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
-
+const add = event =>{
+  
+}
 export default AdditionalFeature;
